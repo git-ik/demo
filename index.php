8193ab4f8b32dbb814aafc7e48e8a053683472b5
XinG <onexing@mail.ru>
2022-04-18 18:13:03 +0300

    add styles for mobile optimizations

--- index.php
+++ index.php
@@ -52,7 +52,6 @@ if (empty($errors['system'])) {
                             </div>
                         </form>
                     </div>
-                    <br>
                 <? } ?>
                 <?php if (empty($_SESSION['auth']) && empty($errors['system'])) { ?>
                     <div class="container">
@@ -127,7 +126,7 @@ if (empty($errors['system'])) {
                 </div>
                 <br>
                 <div id="projectDescription" class="container">
-                    <h3>Задача проекта:</h3>
+                    <h2>Задача проекта:</h2>
                     &nbsp;&nbsp;&nbsp;&nbsp;Привести пример кода с использованием PHP8, MYSQL, JS, HTML, CSS
                     <br>&nbsp;&nbsp;&nbsp;&nbsp;Реализовать демо приложение не являющееся коммерческим <a href="https://ru.wikipedia.org/wiki/%D0%9C%D0%B8%D0%BD%D0%B8%D0%BC%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE_%D0%B6%D0%B8%D0%B7%D0%BD%D0%B5%D1%81%D0%BF%D0%BE%D1%81%D0%BE%D0%B1%D0%BD%D1%8B%D0%B9_%D0%BF%D1%80%D0%BE%D0%B4%D1%83%D0%BA%D1%82">MVP</a>
                     <br>
