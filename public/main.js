32e88067a8013b19ceaf0b01f0812b5943e21ca9
XinG <onexing@mail.ru>
2022-04-22 14:00:09 +0300

    fix resizing canvas

--- public/main.js
+++ public/main.js
@@ -49,106 +49,107 @@ function ajaxLoadDescription(id) {
 /**
  * Render canvas
  */
-var canvas;
+var canvas = [];
 var cWidth;
 var cHeight;
-var ctx;
+var ctx = [];
 var xOffsetLeft;
 var xOffsetRight;
-var xOffset;
+var xOffset = [];
 var rCount;
-var movingDirection;
-var isStarted;
+var movingDirection = [];
 var timeLapse;
-function draw(ctx) {
+function draw(cid) {
     setTimeout(function () {
-        if (xOffset >= xOffsetRight) {
-            movingDirection = 'left';
+        if (xOffset[cid] >= xOffsetRight) {
+            movingDirection[cid] = 'left';
         }
-        if (xOffset <= xOffsetLeft) {
-            movingDirection = 'right';
+        if (xOffset[cid] <= xOffsetLeft) {
+            movingDirection[cid] = 'right';
         }
-        if (movingDirection == 'right') {
-            xOffset = xOffset + 15;
+        if (movingDirection[cid] == 'right') {
+            xOffset[cid] = xOffset[cid] + 15;
         } else {
-            xOffset = xOffset - 15;
+            xOffset[cid] = xOffset[cid] - 15;
         }
 
-        ctx.clearRect(0, 0, cWidth, cHeight);
+        ctx[cid].clearRect(0, 0, cWidth, cHeight);
 
-        ctx.fillStyle = '#000000';
+        ctx[cid].fillStyle = '#000000';
         for (i = 0; i <= rCount; i++) {
-            ctx.fillRect(i * 15, 0, 10, 10);
-            ctx.fillRect(i * 15, 15, 10, 10);
-            ctx.fillRect(i * 15, 30, 10, 10);
-            ctx.fillRect(i * 15, 45, 10, 10);
-            ctx.fillRect(i * 15, 60, 10, 10);
-            ctx.fillRect(i * 15, 75, 10, 10);
+            ctx[cid].fillRect(i * 15, 0, 10, 10);
+            ctx[cid].fillRect(i * 15, 15, 10, 10);
+            ctx[cid].fillRect(i * 15, 30, 10, 10);
+            ctx[cid].fillRect(i * 15, 45, 10, 10);
+            ctx[cid].fillRect(i * 15, 60, 10, 10);
+            ctx[cid].fillRect(i * 15, 75, 10, 10);
         }
 
-        ctx.fillStyle = '#64FF4B';
-        let randY = Math.round(Math.random() * (6 - 0) + 0);
+        ctx[cid].fillStyle = '#64FF4B';
+        let randY = Math.round(Math.random() * 6);
         if (randY > 4) {
-            ctx.fillRect(xOffset, 0, 10, 10);
+            ctx[cid].fillRect(xOffset[cid], 0, 10, 10);
         }
         if (randY == 5) {
-            ctx.fillRect(xOffset, 15, 10, 10);
+            ctx[cid].fillRect(xOffset[cid], 15, 10, 10);
         }
         if (randY < 2) {
-            ctx.fillRect(xOffset, 30, 10, 10);
+            ctx[cid].fillRect(xOffset[cid], 30, 10, 10);
         }
         if (randY == 3) {
-            ctx.fillRect(xOffset, 45, 10, 10);
+            ctx[cid].fillRect(xOffset[cid], 45, 10, 10);
         }
         if (randY == 0) {
-            ctx.fillRect(xOffset, 60, 10, 10);
+            ctx[cid].fillRect(xOffset[cid], 60, 10, 10);
         }
         if (randY == 6) {
-            ctx.fillRect(xOffset, 75, 10, 10);
+            ctx[cid].fillRect(xOffset[cid], 75, 10, 10);
         }
 
-        draw(ctx);
+        draw(cid);
     }, 50);
 }
 
-function start(cEl, isStarted) {
+function start(cid, isStarted) {
     if (!isStarted) {
-        canvas = document.getElementById(cEl);
+        canvas[cid] = document.getElementById(cid);
     }
 
-    if (!isStarted && canvas == null) {
+    if (canvas[cid] == null) {
         return;
     }
 
     cWidth = window.innerWidth - 110;
     cHeight = 85;
-    canvas.width = cWidth;
-    canvas.height = cHeight;
-    xOffset = 0;
+    canvas[cid].width = cWidth;
+    canvas[cid].height = cHeight;
+    xOffset[cid] = 0;
     xOffsetLeft = 0;
     rCount = Math.floor(cWidth / 15);
     xOffsetRight = rCount * 15;
 
-    if (canvas !== undefined) {
-        if (!isStarted) {
-            movingDirection = 'right';
-            ctx = canvas.getContext('2d');
-            draw(ctx);
-            isStarted = true;
-        }
-        window.addEventListener('resize', function () {
-            clearTimeout(timeLapse);
-            timeLapse = setTimeout(function () {
-                start('canvas1', true);
-                start('canvas2', true);
-                start('canvas3', true);
-            }, 50);
-        });
+    if (!isStarted) {
+        movingDirection[cid] = 'right';
+        ctx[cid] = canvas[cid].getContext('2d');
+        draw(cid);
     }
+
+    window.addEventListener('resize', function () {
+        clearTimeout(timeLapse);
+        timeLapse = setTimeout(function () {
+            start('canvas1', true);
+            start('canvas2', true);
+            start('canvas3', true);
+        }, 50);
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
     start('canvas1', false);
-    start('canvas2', false);
-    start('canvas3', false);
+    setTimeout(function () {
+        start('canvas2', false);
+    }, Math.round(Math.random() * 2000));
+    setTimeout(function () {
+        start('canvas3', false);
+    }, Math.round(Math.random() * (4000 - 2000) + 2000));
 });
